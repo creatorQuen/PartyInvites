139368a6277b1287e64f2efb6838a3c8598ab26d
machine.steam@yandex.ru <machine.steam@yandex.ru>
2020-10-01 19:07:21 +0300

    Добавьте файлы проекта.

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using PartyInvites.Models;
+using System.Linq;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.StartScreen = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View("MyView");
+        }
+
+        //public ViewResult RsvpForm()
+        //{
+        //    return View();
+        //}
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            // Что сделать: созранить ответ от гостя
+            // Все, что необходимо сделать с данными формы, отправленными в запро­се - передать 
+            // методу Repository.AddResponse() в качестве аргумента объ­ект GuestResponse, 
+            // который был передан методу действия, чтобы ответ мог быть cохранен.
+
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                // Обнаружена ошибка проверки достоверности.
+                return View();
+            }
+        }
+        
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
+    }
+}
---
+++ PartyInvites/Models/Repository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace PartyInvites.Models
+{
+    // Класс Repository и его члены объявлены статическими, чтобы облегчить со­хранение 
+    // и извлечение данных в разных местах приложения. НО напрактике делают внедрение зависимостей.
+    public static class Repository
+    {
+        private static List<GuestResponse> responses = new List<GuestResponse>();
+
+        public static IEnumerable<GuestResponse> Responses { get { return responses; } }
+
+        public static void AddResponse(GuestResponse response)
+        {
+            responses.Add(response);
+        }
+    }
+}
